7bed7a288e78e4a3cd001be41d3d1d17f2cd5e45
unknown <gustavobaraldi05@gmail.com>
2025-05-15 11:03:38 -0300

    Criação do estilo do site

--- paginasweb/urls.py
+++ paginasweb/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from .views import IndexView, SobreView
-
+################## IMPORT CREATE
+from .views import UsuarioCreate, CameraCreate, SistemaSegurancaCreate,  ProcessadorIACreate, DispositivoAutomacaoCreate, NotificacaoCreate
+################# IMPORT UPDATE
+from .views import UsuarioUpdate, CameraUpdate, DispositivoAutomacaoUpdate, ProcessadorIAUpdate, SistemaSegurancaUpdate, NotificacaoUpdate
+################## IMPORT DELETE
+from .views import UsuarioDelete, CameraDelete, SistemaSegurancaDelete, ProcessadorIADelete, DispositivoAutomacaoDelete, NotificacaoDelete
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),  # URL para a página inicial
     path('sobre/', SobreView.as_view(), name='sobre'),  # URL para a página "Sobre"
@@ -10,4 +15,18 @@ urlpatterns = [
     path('adicionar/processador-ia/', ProcessadorIACreate.as_view(), name='inserir-processador-ia'),
     path('adicionar/dispositivo-automacao/', DispositivoAutomacaoCreate.as_view(), name='inserir-dispositivo-automacao'),
     path('adicionar/notificacao/', NotificacaoCreate.as_view(), name='inserir-notificacao'),
+    #######
+    path('atualizar/Usuario/', UsuarioUpdate.as_view(), name='Atualizar-usuario'),
+    path('atualizar/Camera/', CameraUpdate.as_view(), name='Atualizar-Camera'),
+    path('atualizar/SistemaSegurança/', ProcessadorIAUpdate.as_view(), name='Atualizar-SistemaSegurança'),
+    path('atualizar/Processador-ia/', SistemaSegurancaUpdate.as_view(), name='Atualizar-Processador-ia'),
+    path('atualizar/Dispositivo-automacao/', DispositivoAutomacaoUpdate.as_view(), name='Atualizar-Dispositivo-automacao'),
+    path('atualizar/notificacao/', NotificacaoUpdate.as_view(), name='Atualizar-notificacao'),   
+    #######
+    path('Deletar/Usuario/', UsuarioDelete.as_view(), name='Deletar-Usuario'),
+    path('Deletar/Camera/', CameraDelete.as_view(), name='Deletar-Camera'),
+    path('Deletar/SistemaSegurança/', ProcessadorIADelete.as_view(), name='Deletar-SistemaSegurança'),
+    path('Deletar/Processador-ia/', SistemaSegurancaDelete.as_view(), name='Deletar-Processador-ia'),
+    path('Deletar/Dispositivo-automacao/', DispositivoAutomacaoDelete.as_view(), name='Deletar-Dispositivo-automacao'),
+    path('Deletar/notificacao/', NotificacaoDelete.as_view(), name='Deletar-notificacao'),   
 ]
--- paginasweb/views.py
+++ paginasweb/views.py
@@ -14,6 +14,10 @@ class UsuarioCreate(CreateView):
     template_name = 'paginasweb/form.html'
     fields = ['nome']
     success_url = reverse_lazy('index')
+    extra_context = {
+        'titulo' : 'Cadastrar Usuario',
+        'botao' : 'Cadastrar',
+    }
 
 class CameraCreate(CreateView):
     model = Camera
@@ -45,3 +49,85 @@ class NotificacaoCreate(CreateView):
     fields = ['tipo']
     success_url = reverse_lazy('index')
 
+################################## UPDATE
+class UsuarioUpdate(UpdateView):
+    model = Usuario
+    template_name = 'paginasweb/form.html'
+    fields = ['nome']
+    success_url = reverse_lazy('index')
+    extra_context = {
+        'titulo' : 'Atualização de dados do Usuario',
+        'botao' : 'Salvar',
+    }
+
+class CameraUpdate(UpdateView):
+    model = Camera
+    template_name = 'paginasweb/form.html'
+    fields = ['ip', 'status']  
+    success_url = reverse_lazy('index')
+
+class ProcessadorIAUpdate(UpdateView):
+    model = Processador_IA
+    template_name = 'paginasweb/form.html'
+    fields = ['modelo', 'confiabilidade']
+    success_url = reverse_lazy('index')
+
+class DispositivoAutomacaoUpdate(UpdateView):
+    model = Dispositivo_Automacao
+    template_name = 'paginasweb/form.html'
+    fields = ['tipo', 'estado']
+    success_url = reverse_lazy('index')
+
+class SistemaSegurancaUpdate(UpdateView):
+    model = Sistema_Seguranca
+    template_name = 'paginasweb/form.html'
+    fields = ['nome']  
+    success_url = reverse_lazy('index')
+
+class NotificacaoUpdate(UpdateView):
+    model = Notificacao
+    template_name = 'paginasweb/form.html'
+    fields = ['tipo']
+    success_url = reverse_lazy('index')
+
+######################################### DELETE
+
+class UsuarioDelete(DeleteView):
+    model = Usuario
+    template_name = 'paginasweb/form.html'
+    fields = ['nome']
+    success_url = reverse_lazy('index')
+    extra_context = {
+        'titulo' : 'Atualização de dados do Usuario',
+        'botao' : 'Salvar',
+    }
+
+class CameraDelete(DeleteView):
+    model = Camera
+    template_name = 'paginasweb/form.html'
+    fields = ['ip', 'status']  
+    success_url = reverse_lazy('index')
+
+class ProcessadorIADelete(DeleteView):
+    model = Processador_IA
+    template_name = 'paginasweb/form.html'
+    fields = ['modelo', 'confiabilidade']
+    success_url = reverse_lazy('index')
+
+class DispositivoAutomacaoDelete(DeleteView):
+    model = Dispositivo_Automacao
+    template_name = 'paginasweb/form.html'
+    fields = ['tipo', 'estado']
+    success_url = reverse_lazy('index')
+
+class NotificacaoDelete(DeleteView):
+    model = Notificacao
+    template_name = 'paginasweb/form.html'
+    fields = ['tipo']
+    success_url = reverse_lazy('index')
+
+class SistemaSegurancaDelete(DeleteView):
+    model = Sistema_Seguranca
+    template_name = 'paginasweb/form.html'
+    fields = ['tipo', 'estado']
+    success_url = reverse_lazy('index')
